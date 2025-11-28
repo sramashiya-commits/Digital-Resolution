8516d974a47f6365f0bd6d73356fa397e81b8c07
sramashiya-commits <sramashiya@gmail.com>
2025-11-28 13:28:11 +0200

    Add main.py entrypoint

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Entry point for Digital-Resolution.
+"""
+
+import sys
+
+def main():
+    """Main entrypoint."""
+    print("Hello from Digital-Resolution")
+
+if __name__ == "__main__":
+    main()
